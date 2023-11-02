cb5081dc6a880fa4f9db23937c5d66eac17fc33d
Abolfazltabatabaee <149058638+Abolfazltabatabaee@users.noreply.github.com>
2023-11-02 23:48:19 +0330

    Add files via upload

---
+++ 1_7537315517.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {//abolfazlabatabaee
+                int a = 1;
+                int b = 0;
+                Console.WriteLine("enter a num mr hazfi");
+                int n;
+
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.Write(b);
+                for (int i = 0; i < n; i++)
+                {
+
+                    a = a + b;
+                    Console.Write(a);
+                    Console.Write(" ");
+                    b = a + b;
+                    Console.Write(b);
+                    Console.Write(" ");
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
